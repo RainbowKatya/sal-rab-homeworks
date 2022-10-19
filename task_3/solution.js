375270dccf34585285294b8fab40b1a4012957bc
Katya Raduga <katya.radugaa@gmail.com>
2022-10-19 23:45:38 +0300

    task_3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -18,7 +18,7 @@ function renderCartItem(item) {
     itemCountText = itemCountText + product.count; // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
     itemCountText = itemCountText + " × "; // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText = itemCountText + product.price; // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText = itemCountText + " ₽ = " ; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
+    itemCountText = itemCountText + " ₽ = "; // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     let sum = product.count * product.price; // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
     itemCountText = itemCountText + sum; // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText = itemCountText + " ₽"; // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
